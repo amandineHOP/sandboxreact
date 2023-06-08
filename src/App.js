dd0e6b65da0f6461919f9c0ee8372c3bf8f8a8f8
Amandine Huret <amandine.huret@gmail.com>
2023-06-08 18:12:46 +0200

    internationalisation + bouton switch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
 
+import { useTranslation, Trans } from 'react-i18next';
+
+const lngs = {
+    en: { nativeName: 'English' },
+    fr: { nativeName: 'Francais' }
+};
 function App() {
+
+    const { i18n } = useTranslation();
   return (
     <div className="App">
       <header className="App-header">
+          <div>
+              {Object.keys(lngs).map((lng) => (
+                  <button key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit" onClick={() => i18n.changeLanguage(lng)}>
+                      {lngs[lng].nativeName}
+                  </button>
+              ))}
+          </div>
         <p>
-         Ma premiere appli React pour le Chantier IA signée Amandine, ChatGPT et Copilot! :-)
+                <Trans i18nKey="description.part1">
+             Ma premiere appli React pour le Chantier IA signée Amandine, ChatGPT et Copilot! :-)
+                </Trans>
         </p>
         <a
           className="App-link"
