5615ba073b75d68b85c6b8b9913586cd77f38961
amandineHOP <134275636+amandineHOP@users.noreply.github.com>
2023-06-09 14:31:36 +0200

    Feat/install axios+requete basique (#2)
    * feat/RequeteBasique_fail
    
    * requeteOk+commentairesCopilot
    
    * feat/requeteBasique_boutonAppel
    
    ---------
    
    Co-authored-by: Amandine Huret <amandine.huret@gmail.com>

--- src/i18n.js
+++ src/i18n.js
@@ -1,5 +1,5 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import {initReactI18next} from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
@@ -20,14 +20,22 @@ i18n
             en: {
                 translation: {
                     description: {
-                        part1: 'My first appli with React for the Chantier IA , by Amandine, ChatGPT and Copilot! :-)'
+                        part1: 'Welcome in my first appli with React for the Chantier IA, by Amandine, ChatGPT and Copilot!\n',
+                        part2: 'Clicking on the button, you will see in the console all the asteroids which are near the Earth today.',
+                        part3: 'For now, it\'s quite hard to decrypt but we are trying to make better as soon as possible. Be patient ;-)',
+                        part4: 'Discover the NASA website'
                     }
                 }
             },
             fr: {
                 translation: {
                     description: {
-                        part1: 'Ma premiere appli React pour le Chantier IA signée Amandine, ChatGPT et Copilot! :-)'
+                        part1: 'Bienvenue dans ma premiere appli React pour le Chantier IA, signée Amandine, ChatGPT et Copilot!',
+                        part2: 'En cliquant sur le bouton ci-dessous, vous verrez dans la console de cette fenetre l\'ensemble des\n' +
+                            '                             asteroides qui sont proches de la Terre aujourd\'hui.',
+                        part3: 'Pour l \'instant c\'est pas facile à déchiffrer mais on va tenter de faire un joli tableau tout beau tout propre dans la fenetre du\n' +
+                            '                             navigateur. Un peu de patience ;-)',
+                        part4: 'Découvrir le site de la NASA'
                     }
                 }
             }
