d09d3d3b736f02f15c8744617fca27dfb873575f
Amandine Huret <amandine.huret@gmail.com>
2023-06-09 11:24:08 +0200

    feat/RequeteBasique_fail

--- src/App.js
+++ src/App.js
@@ -1,40 +1,88 @@
 import './App.css';
 
-import { useTranslation, Trans } from 'react-i18next';
+import {useTranslation, Trans} from 'react-i18next';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 
 const lngs = {
-    en: { nativeName: 'English' },
-    fr: { nativeName: 'Francais' }
+    en: {nativeName: 'English'},
+    fr: {nativeName: 'Francais'}
 };
+
 function App() {
 
-    const { i18n } = useTranslation();
-  return (
-    <div className="App">
-      <header className="App-header">
-          <div>
-              {Object.keys(lngs).map((lng) => (
-                  <button key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit" onClick={() => i18n.changeLanguage(lng)}>
-                      {lngs[lng].nativeName}
-                  </button>
-              ))}
-          </div>
-        <p>
-                <Trans i18nKey="description.part1">
-             Ma premiere appli React pour le Chantier IA signée Amandine, ChatGPT et Copilot! :-)
-                </Trans>
-        </p>
-        <a
-          className="App-link"
-          href="https://www.groupeonepoint.com/fr"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         Découvrir Onepoint
-        </a>
-      </header>
-    </div>
-  );
-}
+    const {i18n} = useTranslation();
 
-export default App;
+    const [aujourdhui, setAujourdhui] = useState('');
+    const [hier, setHier] = useState('');
+
+    useEffect(() => {
+        const currentDate = new Date(); // Obtient la date d'aujourd'hui
+        const previousDate = new Date();
+        previousDate.setDate(currentDate.getDate() - 1); // Obtient la date d'hier
+
+        //definir aujourd hui et hier et les formatter en aaaa/mm/jj pour les passer en parametre de l url
+        setAujourdhui(currentDate.toISOString().split('T')[0]);
+        setHier(previousDate.toISOString().split('T')[0]);
+
+            }, []);
+
+        useEffect(() => {
+            const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${encodeURIComponent(hier)}&end_date=${encodeURIComponent(aujourdhui)}&api_key=DEMO_KEY`;
+
+            axios
+                .get(url)
+                .then(response => {
+                    console.log(response.data);
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        }, [aujourdhui, hier]);
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={"https://www.nasa.gov/sites/all/themes/custom/nasatwo/images/nasa-logo.svg"}
+                         alt="Nasa"/>
+                    <div>
+                        {Object.keys(lngs).map((lng) => (
+                            <button key={lng} style={{fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal'}}
+                                    type="submit" onClick={() => i18n.changeLanguage(lng)}>
+                                {lngs[lng].nativeName}
+                            </button>
+                        ))}
+                    </div>
+                    <p>
+                        <Trans i18nKey="description.part1">
+                            Ma premiere appli React pour le Chantier IA signée Amandine, ChatGPT et Copilot! :-)
+                        </Trans>
+                    </p>
+                    <p>
+                         <Trans i18nKey="description.part2">
+                             Nous sommes le:
+                         </Trans>
+                    </p>
+                    <p>{aujourdhui}</p>
+                    <p>
+                        <Trans i18nKey="description.part3">
+                            Vous pouvez voir dans la console de cette fenetre l'ensemble des
+                            asteroides qui sont proches de la Terre aujourd'hui. Pour l 'instant c'est pas facile à
+                            déchiffrer mais on va tenter de faire un joli tableau tout beau tout propre dans la fenetre du
+                            navigateur. Un peu de patience !
+                        </Trans>
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://www.nasa.gov/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Découvrir le site de la NASA
+                    </a>
+                </header>
+            </div>
+        );
+    }
+
+    export default App;
--- src/i18n.js
+++ src/i18n.js
@@ -1,5 +1,5 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import {initReactI18next} from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
@@ -20,14 +20,23 @@ i18n
             en: {
                 translation: {
                     description: {
-                        part1: 'My first appli with React for the Chantier IA , by Amandine, ChatGPT and Copilot! :-)'
+                        part1: 'Welcome in my first appli with React for the Chantier IA , by Amandine, ChatGPT and Copilot! :-)\n',
+                        part2: 'We are the ',
+                        part3: 'You can see in the console all the\n' +
+                            '                             asteroids which are near the Earth today. For now, it\'s quite hard to read but we are trying to make better\n' +
+                            '                             as soon as possible. Be patient !'
                     }
                 }
             },
             fr: {
                 translation: {
                     description: {
-                        part1: 'Ma premiere appli React pour le Chantier IA signée Amandine, ChatGPT et Copilot! :-)'
+                        part1: 'Bienvenue dans ma premiere appli React pour le Chantier IA signée Amandine, ChatGPT et Copilot! :-)',
+                        part2: 'Nous sommes le ',
+                        part3: 'Vous pouvez voir dans la console de cette fenetre l\'ensemble des\n' +
+                            '                             asteroides qui sont proches de la Terre aujourd\'hui. Pour l \'instant c\'est pas facile à\n' +
+                            '                             déchiffrer mais on va tenter de faire un joli tableau tout beau tout propre dans la fenetre du\n' +
+                            '                             navigateur. Un peu de patience !'
                     }
                 }
             }
